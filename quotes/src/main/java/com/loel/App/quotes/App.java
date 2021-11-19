fdd95a6031cccb4734f2f2017b9f33fe05409130
NerfedWizard <loelnelson@gmail.com>
2021-11-19 14:06:44 -0600

    Initial

---
+++ quotes/src/main/java/com/loel/App/quotes/App.java
@@ -0,0 +1,61 @@
+package com.loel.App.quotes;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+/**
+ * Rough draft for making the app for jones to see quotes
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		
+		
+		
+//		List<List<String>> records = new ArrayList<List<String>>();
+//		try (CSVReader cread = new CSVReader(new FileReader("/home/loel/Documents/InspirQuotes/quotes.csv"));) {
+//			String[] list = null;
+//			while ((list = cread.readNext()) != null) {
+//				records.add(Arrays.asList(list));
+//			}
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (CsvValidationException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		/** The odd numbers are the author and the even numbers are the quote */
+////		for (int i = 0; i < records.size(); i++) {
+////			System.out.println(records.get(1));
+////		}
+////		for (List<String> s : records) {
+////			System.out.println(s);
+////			for (String t : s) {
+//////				System.out.println(t);
+////			}
+////		}
+//		Map<String, List<String>> quotesMap = new HashMap<>();
+//		for (List<String> s : records) {
+//
+//			for (String t : s) {
+////				quotesMap.put(s.toString(), t);
+////				System.out.println(t);
+//			}
+//		}
+//		System.out.println(quotesMap.get("Thomas Edison"));
+		
+	}
+}
