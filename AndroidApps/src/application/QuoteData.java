ba1150897cba0733cfddbcd939ffdf4bbcec9d03
NerfedWizard <loelnelson@gmail.com>
2021-11-24 17:57:02 -0600

    Dell

--- AndroidApps/src/application/QuoteData.java
+++ AndroidApps/src/application/QuoteData.java
@@ -18,7 +18,7 @@ public class QuoteData {
 	public void quotes() throws SQLException {
 		list = new ArrayList<Author>();
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/funtimes", "root", "root");
-				PreparedStatement preparedStatement = conn.prepareStatement("select *from quotes_first");) {
+				PreparedStatement preparedStatement = conn.prepareStatement("select *from quote_first");) {
 			ResultSet rs = preparedStatement.executeQuery();
 			while (rs.next()) {
 				String author = rs.getString(1);
